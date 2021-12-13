7533d3623ec307cfae033bf0c4e0c58e479bc7ad
eva-burns <evasophia@gmail.com>
2021-12-13 13:02:39 -0500

    Aalens markdown

--- convert_list.R
+++ convert_list.R
@@ -1,8 +1,16 @@
+
+# Cox and Aalen's regressions need to have data in a readable format for them.
+# This takes data where there is one organism per row and converts it to be 
+# one measurement per row.
+
+# data: dataset before being converted.  Each row represents an organism's 
+# trait measurements until they die
 convert_list <- function(data){
-  time <- vector()
-  status<- vector()
-  trait <- vector()
-  ids <- vector()
+  time <- vector() # times where each trait value was measured
+  status<- vector() # status: 0 alive, 1 dead
+  trait <- vector() # value of trait at a certain time
+  ids <- vector() # organism id used for clustering
+  
   for (row in 1:nrow(data)) {
     row_data <- data[row,][!is.na(data[row,])]
     row_data <- row_data[2:length(row_data)]
